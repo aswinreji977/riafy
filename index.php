01172826a600869eef5ca6c2fa60d8d78eaf93eb
aswinreji977 <74725737+aswinreji977@users.noreply.github.com>
2021-06-14 19:39:44 +0530

    u

--- index.php
+++ index.php
@@ -1,83 +1,109 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>NSE</title>
-	</head>
-	<body>
-		<h2>Stocks</h2>
-		<div align="center">
-			The easiest way to buy and sell stocks
-			Stock analysis and screening tool
-		</div>
-		<div align="center">
-			<form action="" method="GET">
-				<label for="cmp"></label>
-				<input list="cmps" name="cmp" id="cmp">
-				<datalist id="cmps">
-					<?php
-								include ("config.php");
-							$handle = mysqli_connect($host, $dbuser, $dbpass, $dbname);
-							if (mysqli_connect_error())
-							{
-								echo "Unable to connect to database!";
-							}
-							else
-							{
 
-							  
-								$result = mysqli_query($handle, "Select * from stocks");
-								if (mysqli_num_rows($result) == false)
-								{
-									echo "<option value='No companies available'>";
-								}
-								else
-								{
-									while ($row = mysqli_fetch_array($result))
-									{
-										$name = $row[1];
-										echo "<option value=$name>";
-									}
-									
-								}
-							}
-						?>
+<head>
+    <title>NSE</title>
+    <style>
+        .value{
+        			color:red;
+        			font-weight:bold;
+        		}
+    </style>
+</head>
+
+<body>
+    <h2>Stocks</h2>
+    <div align="center">
+        <b size="55px">The easiest way to buy and sell stocks</b>
+        <p size="24px">Stock analysis and screening tool for investors in India
+    </div>
+    <div align="center">
+        <form action="" method="GET">
+            <label for="cmp">Search</label>
+            <input list="cmps" name="cmp" id="cmp" onchange="this.form.submit()">
+            <datalist id="cmps">
+					<?php
+					 include "config.php";
+					 $handle = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+					 if (mysqli_connect_error()){
+						 echo "Unable to connect to database!";
+					 } 
+					 else {
+						 $result = mysqli_query($handle, "Select * from stocks");
+						 if (mysqli_num_rows($result) == false) {
+							 echo "<option value='No companies available'>";
+						 } 
+						 else {
+							 while ($row = mysqli_fetch_array($result)) {
+								 $name = $row[1];
+								 echo "<option value='$name'>";
+							 }
+						 }
+					 }
+					?>
 					
 	  
 				</datalist>
-			<input type="submit" name="submit">
-			</form>
-		</div>
-	
-<?php
-if(isset($_GET["submit"])){
-$value=$_GET["cmp"];
-				
- $query = mysqli_query($handle, "Select * from stocks where Name='$value'");
-    if($query){
-        while ($row = mysqli_fetch_array($query))
-        {
-            $mc = $row[3];
-            $dy = $row[7];
-            $de= $row[10];
-			$cp=$row[4];
-			$roc=$row[8];
-			$eps=$row[11];
-			$stocks=$row[6];
-			$roe=$row[9];
-			$res=$row[12];
-			$debt=$row[13];
-		}
-	
-echo"<div align='center'>
-	<h3>$value</h3>
-	<table> 
-	<tr>
-	<td>Market Cap</td>
-	<td>$mc</td>
-	</div>";
+            <input type="submit" name="submit">
+        </form>
+    </div>
 
-	}
-}
+    <?php 
+	if (isset($_GET["submit"])) {
+    $value = $_GET["cmp"];
+
+    $query = mysqli_query($handle, "Select * from stocks where Name='$value'");
+    if (mysqli_num_rows($query) == true) {
+        while ($row = mysqli_fetch_array($query)) {
+            $value = $row[1];
+            $mc = $row[3];
+            $dy = $row[5];
+            $de = $row[8];
+            $cp = $row[2];
+            $roc = $row[6];
+            $eps = $row[9];
+            $stock = $row[4];
+            $roe = $row[7];
+            $res = $row[10];
+            $debt = $row[11];
+        }
+
+        echo "<div align='center'>
+				<table cellspacing='20px'>
+					<caption style='text-align:left'><b>$value</b></caption>
+						<tr>
+							<td>Market Cap</td>
+							<td class='value'>₹ $mc</td>
+							<td>Dividend Yield</td>
+							<td class='value'>$dy%</td>
+							<td>Debt Equality</td>
+							<td class='value'>$de%</td>
+						</tr>
+						<tr>
+							<td>Current Price</td>
+							<td class='value'>₹ $cp</td>
+							<td>ROCE</td>
+							<td  class='value'>$roc%</td>
+							<td>EPS</td>
+							<td class='value'>₹ $eps</td>
+						</tr>
+						<tr>
+							<td>Stock PE</td>
+							<td class='value'>$stock%</td>
+							<td>ROE</td>
+							<td class='value'>$roe%</td>
+							<td>Reserves</td>
+							<td class='value'>₹ $res</td>
+						</tr>
+						<tr>
+						<td>Debt</td>
+						<td class='value'>₹ $debt</td>
+						</tr>
+				</table>
+			</div>";
+    }
+	} 
 ?>
 </body>
-	</html>
+
+</html>
